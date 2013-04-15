71869c948404c53d900131c6b0b0ee1cd3cf7cb7
Tobias Grundel <tobias.grundel@hotmail.com>
2013-04-15 16:19:22 +0200

    D3DMathUtility kommer här för jag är super bäst...

--- Lilla spelprojektet/Code/D3DMathUtility.h
+++ Lilla spelprojektet/Code/D3DMathUtility.h
@@ -1,1 +1,51 @@
-//Fuck yeah I'm on GitHub beetches!!!12343
+#ifndef D3DMATHUTILITY_H
+#define D3DMATHUTILITY_H
+#include <D3DX10math.h>
+
+#define Vec2 D3DXVECTOR2
+#define Vec3 D3DXVECTOR3
+#define Vec4 D3DXVECTOR4
+#define Matrix D3DXMATRIX
+
+#define PI (3.14159265358979323846f)
+
+struct MeshPUV
+{
+	Vec3 pos;
+	Vec2 uv;
+
+	MeshPUV()
+	{
+		this->pos = Vec3(0.0f, 0.0f, 0.0f);
+		this->uv	= Vec2(0.0f, 0.0f);
+	}
+
+	MeshPUV(Vec3 pos, Vec2 uv)
+	{
+		this->pos	= pos;
+		this->uv		= uv;
+	}
+};
+
+struct MeshPNUV
+{
+	Vec3 pos;
+	Vec3 normal;
+	Vec2 uv;
+
+	MeshPNUV()
+	{
+		this->pos		= Vec3(0.0f, 0.0f, 0.0f);
+		this->normal		= Vec3(0.0f, 0.0f, 0.0f);
+		this->uv			= Vec2(0.0f, 0.0f);
+	}
+	
+	MeshPNUV(Vec3 pos, Vec3 normal, Vec2 uv)
+	{
+		this->pos		= pos;
+		this->normal		= normal;
+		this->uv			= uv;
+	}
+};
+
+#endif
