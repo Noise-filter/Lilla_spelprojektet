bef6642d5617f28a5e701e414cf3e4277993d984
Erik Persson <erikpersson583@gmail.com>
2013-04-15 15:38:26 +0200

    grejer
    massa grejer

--- Lilla spelprojektet/Code/Level.cpp
+++ Lilla spelprojektet/Code/Level.cpp
@@ -3,13 +3,38 @@
 
 Level::Level(void)
 {
-
+	this->mapSize = 0;
+	this->nodes = NULL;
 }
 
+bool Level::init(int mapSize)
+{
+	this->mapSize = mapSize;
+
+	nodes = new Node*[mapSize];
+	for(int i = 0; i < mapSize; i++)
+	{
+		nodes[i] = new Node[mapSize];
+	}
+
+	for(int i = 0; i < mapSize; i++)
+	{
+		for(int j = 0; j < mapSize; j++)
+		{
+			nodes[i][j] = Node();
+		}
+	}
+
+	return true;
+}
 
 Level::~Level(void)
 {
-
+	for(int i = 0; i < this->mapSize; i++)
+	{
+		SAFE_DELETE(nodes[i]);
+	}
+	SAFE_DELETE_ARRAY(nodes);
 }
 
 int Level::update(float dt)
--- Lilla spelprojektet/Code/Level.h
+++ Lilla spelprojektet/Code/Level.h
@@ -1,14 +1,18 @@
 #pragma once
 #include "stdafx.h"
+#include "Node.h"
 class Level
 {
 public:
 	Level(void);
 	~Level(void);
 
+	bool init(int mapSize);
+
 	RenderData* getRenderData();
 	int update(float dt);
 private:
-
+	Node** nodes;
+	int mapSize;
 };
 
