a08fd892480a83353625dfbc14050b840baad571
Tobias Grundel <tobias.grundel@hotmail.com>
2013-04-23 15:40:24 +0200

    bla

--- Lilla spelprojektet/Code/GeometryManager.h
+++ Lilla spelprojektet/Code/GeometryManager.h
@@ -33,6 +33,7 @@ class GeometryManager
 		void applyBuffer(ID3D11DeviceContext *dc, RENDERDATA *obj, D3D_PRIMITIVE_TOPOLOGY topology, UINT32 misc = 0);
 		void updateBuffer(ID3D11DeviceContext *dc, std::vector<RENDERDATA*> data, int index);
 		void debugApplyBuffer(ID3D11DeviceContext *dc, ID3D11Device *d);
+		void geoDebugBuffer(ID3D11DeviceContext *dc, ID3D11Device *d);
 };
 
 #endif
