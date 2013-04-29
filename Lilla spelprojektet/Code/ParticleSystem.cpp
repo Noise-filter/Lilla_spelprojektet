ee8bbe85a960f914bce0d8652609f76ca417d527
Pontus Fransson <pontus.fransson92@gmail.com>
2013-04-29 10:13:24 +0200

    La till removeParticlePolicy

--- Lilla spelprojektet/Code/ParticleSystem.cpp
+++ Lilla spelprojektet/Code/ParticleSystem.cpp
@@ -60,15 +60,26 @@ vector<vector<VertexColor>> ParticleSystem::getVertexData()
 }
 
 
-Trail* ParticleSystem::addTrail(D3DXVECTOR3 color, D3DXVECTOR3 position , int intensity, int timeToLive, float velocity, float lengthX, float lengthY, float lengthZ)
+ParticlePolicy* ParticleSystem::addTrail(D3DXVECTOR3 color, D3DXVECTOR3 position , int intensity, int timeToLive, float velocity, float lengthX, float lengthY, float lengthZ)
 {
 	Trail* temp = new Trail(color, position, intensity, timeToLive, velocity, lengthX, lengthY, lengthZ);
 	particlePolicies.push_back(temp);
 
-	return temp;
+	return particlePolicies.at(particlePolicies.size()-1);
 }
 
-bool ParticleSystem::removePolicy() // kompletera med hur detta ska ske, hur removar man den policy man vill åt?(id? string namn?)
+bool ParticleSystem::removePolicy(ParticlePolicy* temp) // kompletera med hur detta ska ske, hur removar man den policy man vill åt?(id? string namn?)
 {
+	for(int i = 0; i < particlePolicies.size(); i++)
+	{
+		if(particlePolicies.at(i) == temp)
+		{
+			delete particlePolicies.at(i);
+			particlePolicies.at(i) = NULL;
+			particlePolicies.erase(particlePolicies.begin() + i);
+			temp = NULL;
+		}
+	}
+
 	return true;
 }
