72b25e86b127b81893c47a560466df163eceaaf6
Pontus Fransson <pontus.fransson92@gmail.com>
2013-05-03 09:27:41 +0200

    D3DHandler

--- Lilla spelprojektet/Code/D3D11Handler.h
+++ Lilla spelprojektet/Code/D3D11Handler.h
@@ -2,30 +2,57 @@
 #define D3D11HANDLER_H
 
 #include "D3DGraphicUtility.h"
-#include "WinHandler.h"
+#include "D3DMathUtility.h"
+#include "WindowsUtility.h"
 #include "Shader.h"
 
-class D3D11Handler
+class D3D11Handler		
 {
 	private:
+		D3D11_VIEWPORT			viewPort;
+		ID3D11RenderTargetView	*pRenderTargetView;
+
+		ID3D11Texture2D			*pDepthStencil;
+		ID3D11DepthStencilView	*pDepthStencilView;
+
+		//Deferred targets
+		ID3D11Texture2D				**pDeferredTargets;
+		ID3D11RenderTargetView		**pMultipleRTVs;
+		ID3D11ShaderResourceView	**pMultipleSRVs;
+		ID3D11ShaderResourceView	**pNullSRVs;
+		ID3D11DepthStencilView		*pDSVDeferred;
+		int							 iNrOfDeferred;
+		std::vector<Shader*>         vShaders;
+
+	private:
+		bool initSwapChainAndDevice(HWND hWnd);
+		void initAndSetViewPort();
+		bool initDepthStencil();
+		bool initRenderTargetView();
+		bool initShaders();
+
+		//Deferred initiation
+		bool initDeferred();
+		bool bindResources(D3D11_TEXTURE2D_DESC &texDesc);
+		//--------------
 
 
-	protected:
-		D3D11_VIEWPORT          viewPort;
-		char* FeatureLevelToString(D3D_FEATURE_LEVEL featureLevel);
 
 	public:
-		ID3D11Device*			device;
-		ID3D11DeviceContext*	deviceContext;
-		IDXGISwapChain*         swapChain;	
-		ID3D11RenderTargetView* renderTargetView;	
-		ID3D11Texture2D*        depthStencil; 
-		ID3D11DepthStencilView* depthStencilView;
+		ID3D11Device		*pDevice;
+		ID3D11DeviceContext *pDeviceContext;
+		IDXGISwapChain		*pSwapChain;
 
+	public:
 		D3D11Handler();
-		~D3D11Handler();
-		HRESULT InitDirect3D(HWND hWnd);
-		void resetRT();
+		virtual~D3D11Handler();
+
+		bool initDirect3D(HWND hWnd);
+
+		Shader *setPass(PASS_STATE pass);
+
+		void clearAndBindRenderTarget();
+
 };
 
 #endif
